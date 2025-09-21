f1fde7427a0866f4d7b57908935ca71e020fff67
xuebinyang <1519959584@qq.com>
2025-09-22 00:39:21 +0800

    important change

--- docs/projects/en-ge-model.md
+++
@@ -1,3 +0,0 @@
-# 一、中德翻译模型训练项目
-
-sorry, my website just built right before, i will upload files soon.
--- site/javascripts/mathjax_init.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  if (typeof MathJax !== 'undefined') {
-    MathJax.typesetPromise().then(() => {
-      console.log('MathJax typesetting triggered on page load');
-    }).catch((err) => console.error('MathJax typesetting error:', err));
-  }
-});
